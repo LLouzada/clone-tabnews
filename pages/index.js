cc21c367a87687b75278d6e89a3986236c5019ec
LLouzada <loumad.soft@gmail.com>
2024-12-26 17:53:52 -0300

    centraliza uma div 😎

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 function Home() {
-  return <div>
-      <h1>Salve Manzinho 😎 </h1>
-      <h2> O Beach Tênis venceu!</h2>
-      <img src="/img/jorginho.png" alt="Jorginho" />
-  </div>
+  return (
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "40%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <h1>Salve Manzinho 😎 </h1>
+        <h2> O Beach Tênis venceu!</h2>
+        <img src="/img/jorginho.png" alt="Jorginho" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
